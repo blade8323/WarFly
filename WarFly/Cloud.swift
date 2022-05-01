ed5faff84f7fd8d2bd4e34dc1a1d97edfc92260e
blade8323 <blade8323@mail.ru>
2022-05-01 21:30:42 +0500

    move clouds and islands

--- WarFly/Cloud.swift
+++ WarFly/Cloud.swift
@@ -17,6 +17,7 @@ final class Cloud: SKSpriteNode, GamebackgroundSpritable {
         cloud.setScale(randomScaleFactor)
         cloud.position = point
         cloud.zPosition = 10
+        cloud.run(move(from: point))
         
         return cloud
     }
@@ -36,4 +37,12 @@ final class Cloud: SKSpriteNode, GamebackgroundSpritable {
         return randomNumber
     }
     
+    fileprivate static func move(from point: CGPoint) -> SKAction {
+        let movePoint = CGPoint(x: point.x, y: -200)
+        let moveDistance = point.y + 200
+        let movementSpeed = CGFloat(15.0)
+        let duration = moveDistance / movementSpeed
+        return SKAction.move(to: movePoint, duration: TimeInterval(duration))
+    }
+
 }
