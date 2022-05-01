e711df95addba76ce69b248404e8f85b18cbf3dc
blade8323 <blade8323@mail.ru>
2022-05-01 22:06:35 +0500

    Movement by CoreMotion

--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -7,8 +7,13 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
 class GameScene: SKScene {
+    
+    var player: SKSpriteNode!
+    let motionmanager = CMMotionManager()
+    var xAcceleration: CGFloat = 0
     
     override func didMove(to view: SKView) {
         let centerScreen = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
@@ -29,5 +34,28 @@ class GameScene: SKScene {
 
         }
         
+        player = PlayerPlane.populate(at: CGPoint(x: screen.size.width / 2, y: 100))
+        self.addChild(player)
+        
+        motionmanager.accelerometerUpdateInterval = 0.2
+        motionmanager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+            if let data = data {
+                let acceleration = data.acceleration
+                self.xAcceleration = CGFloat(acceleration.x) * 0.7 + self.xAcceleration * 0.3
+            }
+        }
     }
+    
+    override func didSimulatePhysics() {
+        super.didSimulatePhysics()
+        
+        player.position.x += xAcceleration * 50
+        if player.position.x <  -70 {
+            player.position.x = self.size.width + 70
+        } else if player.position.x > self.size.width + 70 {
+            player.position.x = -70
+        }
+    }
+    
+    
 }
