e2b27e165a6c7b8d989c3e220a89b5407d1c29fe
blade8323 <blade8323@mail.ru>
2022-05-01 17:22:52 +0500

    adding islands

--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -9,11 +9,19 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-        
+    
     override func didMove(to view: SKView) {
         let centerScreen = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
         let background = Backgound.populateBackground(at: centerScreen)
         background.size = self.size
         self.addChild(background)
+        
+        let screen = UIScreen.main.bounds
+        for _ in 1...5 {
+            let x: CGFloat = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.width)))
+            let y: CGFloat = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.height)))
+            let island = Island.populateIsland(at: CGPoint(x: x, y: y))
+            self.addChild(island)            
+        }
     }
 }
