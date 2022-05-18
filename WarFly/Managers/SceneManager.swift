0de62fb49f46dc40d2992e7cf0a682204a4bbaeb
blade8323 <blade8323@mail.ru>
2022-05-18 21:22:03 +0500

    Scene manager

---
+++ WarFly/Managers/SceneManager.swift
@@ -0,0 +1,13 @@
+//
+//  SceneManager.swift
+//  WarFly
+//
+//  Created by Владислав Соколов on 18.05.2022.
+//
+
+import SpriteKit
+
+class SceneManager {
+    static let shared = SceneManager()
+    var gameScene: GameScene?
+}
